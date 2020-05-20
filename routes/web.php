938ad6529b3e365fab0a4fcb9738ec44e1792eed
javabow <hfarhandika@gmail.com>
2020-05-20 14:44:10 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -29,38 +29,6 @@ Route::get('login-sistem', function () {
 	return view('blog');
 });
 
-Route::get('dosen', 'DosenController@index');
-
-//Route::get('/pegawai/{nama}', 'PegawaiController@index');
-
-//Route::get('/formulir', 'PegawaiController@formulir');
-
-//Route::post('/formulir/proses', 'PegawaiController@proses');
-
-Route::get('/blog', 'BlogController@home');
-
-Route::get('/blog/tentang', 'BlogController@tentang');
-
-Route::get('/blog/kontak', 'BlogController@kontak');
-
-//route CRUD
-Route::get('/pegawai','PegawaiController@index');
-
-Route::get('/pegawai/tambah','PegawaiController@tambah');
-
-Route::post('/pegawai/store','PegawaiController@store');
-
-Route::get('/pegawai/edit/{id}','PegawaiController@edit');
-
-Route::post('/pegawai/update','PegawaiController@update');
-
-Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
-
-Route::get('/pegawai/cari','PegawaiController@cari');
-
-Route::get('/pegawai/input', 'PegawaiController@input');
- 
-Route::post('/pegawai/proses', 'PegawaiController@proses');
 
 //Eloquent
 
