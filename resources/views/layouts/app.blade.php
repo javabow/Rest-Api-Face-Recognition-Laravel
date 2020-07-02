24a2c60377f9a635fb9e1d0afa5cf983d413c33a
javabow <hfarhandika@gmail.com>
2020-07-03 06:14:04 +0700

    change title and remove remember password

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- <title>{{ config('app.name', 'Encoded Face Trainer') }}</title> -->
+    <title>Encoded Face Trainer</title>
 
     <!-- Scripts -->
     <!-- Bootstrap core JavaScript-->
@@ -28,7 +29,8 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <!-- {{ config('app.name', 'Encoded Face') }} -->
+                    Encoded Face
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
